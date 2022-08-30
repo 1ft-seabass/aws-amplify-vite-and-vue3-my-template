0148f917576c3c4bf3b13ad25967a506776e8775
Tanaka Seigo <seigo@1ft-seabass.jp>
2022-08-30 20:30:49 +0900

    src/main.js への Amplify 対応

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+// Amplify Add
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
 createApp(App).mount('#app')
